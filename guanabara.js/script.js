f2cd99f4eeee66945ca0135968b221c4833dff2b
raimundoneto01 <raimundogomes826@gmail.com>
2022-11-22 16:14:11 -0300

    modificando meu curso java script

--- guanabara.js/script.js
+++ guanabara.js/script.js
@@ -4,4 +4,7 @@
  // var nome = window.prompt('Qual é o seu nome?')
  // document.write(' <h1>Seu nome tem $(nome length) letras.</h1> ')
  
- 
+/* window.alert('olá meu projeto');
+ window.confirm('você está gostando do meu projeto?');
+ window.prompt('qual o seu nome?');
+ */
