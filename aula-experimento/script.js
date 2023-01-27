de503ba814fa718a9531e8815673cdaa00153c5e
raimundoneto01 <raimundogomes826@gmail.com>
2023-01-27 16:44:55 -0300

    atualização02

---
+++ aula-experimento/script.js
@@ -0,0 +1,20 @@
+// function somar(value1, value2)
+// {
+//     let soma = value1 + value2 ;
+//     return soma;
+// }
+// function boasvindas(nome = "")
+// {
+//     return "seja bem vindo " + nome;
+// }
+// function tocar(nome = "")
+// {
+//    return "Deixa, Que eu me viro. Deixa, se é dessa maneira, Que você prefere. Dis que me esquece . E não me merece, " + nome;
+// }
+
+
+function tocar(){
+   let tocar = "seja bem vindo ao curso full stack";
+   return tocar;
+   
+}
