1f284cc3661c39e5521cad54dd7cd2f330bc53b4
raimundoneto01 <raimundogomes826@gmail.com>
2023-02-15 19:29:11 -0300

    commit

---
+++ joao-victor/atividade-logica/scripts.js
@@ -0,0 +1,17 @@
+
+    // 1 Questão
+// function boasVindas(){
+//     document.write("Boas Vindas")
+// } boasVindas()
+
+    // 2Questão
+ function calcularIdade(ano){
+    let anoAtual = 2023
+    let somar = anoAtual - ano
+    document.write("A idade é "+ somar +" anos")
+ } 
+ calcularIdade(1993)
+
+     //3Questão...
+ 
+
